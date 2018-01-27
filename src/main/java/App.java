8f807ace01fb86720246b1a0b3d18789a90e78d2
Danyel Murray <danyelmurray1987@gmail.com>
2018-01-26 17:02:22 -0800

    begin to add app routes.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,5 @@
 import com.google.gson.Gson;
+import dao.DrinkDao;
 import dao.Sql2oAlcoholicDao;
 import dao.Sql2oDrinkDao;
 import dao.Sql2oNonAlcoholicDao;
@@ -27,6 +28,24 @@ public class App {
         nonAlcoholicDao = new Sql2oNonAlcoholicDao(sql2o);
         alcoholicDao = new Sql2oAlcoholicDao(sql2o);
         conn = sql2o.open();
+
+
+        //new drink
+        post("/drinks/new", "application/json", (req, res) -> {
+            Drink drink = gson.fromJson(req.body(), Drink.class);
+            drinkDao.add(drink);
+            res.status(201);
+            return "The following was added to the API\n\r" + gson.toJson(drink);
+        });
+
+        //show all drinks
+        get("/drinks", "application/json", (req, res) -> {
+            if (drinkDao.getAll().size() > 0) {
+                return gson.toJson(drinkDao.getAll());
+            } else {
+                return "{\"message\":\"No drinks in database.\"}";
+            }
+        });
 
 
     }
